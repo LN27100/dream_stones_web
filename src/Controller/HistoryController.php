a325fbed7b95d5cfb8fd42178af85946e999536c
LN27100 <heloceinlovehotmail.fr>
2024-03-19 14:19:24 +0100

    ajout bouton suppression commande non validée

--- src/Controller/HistoryController.php
+++ src/Controller/HistoryController.php
@@ -3,20 +3,20 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\ORM\EntityManagerInterface; 
 use App\Entity\Orders;
-use Doctrine\Persistence\ManagerRegistry;
 
 class HistoryController extends AbstractController
 {
-    private $doctrine;
+    private $entityManager;
 
-    // Injecte ManagerRegistry via le constructeur
-    public function __construct(ManagerRegistry $doctrine)
+    // Injecte EntityManagerInterface via le constructeur
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        $this->doctrine = $doctrine;
+        $this->entityManager = $entityManager;
     }
 
     /**
@@ -25,10 +25,30 @@ class HistoryController extends AbstractController
     public function index(): Response
     {
         // Récupére toutes les commandes depuis la base de données
-        $orders = $this->doctrine->getRepository(Orders::class)->findAll();
+        $orders = $this->entityManager->getRepository(Orders::class)->findAll();
 
         return $this->render('orders/historyOrder.html.twig', [
             'orders' => $orders,
         ]);
+    }
+
+    /**
+     * @Route("/history/delete/{id}", name="history_delete", methods={"DELETE"})
+     */
+    public function deleteOrder($id): JsonResponse
+    {
+        // Charge l'entité Orders à partir de l'ID
+        $order = $this->entityManager->getRepository(Orders::class)->find($id);
+
+        // Vérifie si la commande est en attente de validation
+        if ($order && $order->getStatus() === 'en attente de validation') {
+            $entityManager = $this->entityManager;
+            $entityManager->remove($order);
+            $entityManager->flush();
+
+            return new JsonResponse(['message' => 'Commande supprimée avec succès'], Response::HTTP_OK);
+        }
+
+        return new JsonResponse(['error' => 'Impossible de supprimer la commande'], Response::HTTP_BAD_REQUEST);
     }
 }
