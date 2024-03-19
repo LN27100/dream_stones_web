bd9c5fe189700fd538c24e7bb79d0195905fb460
LN27100 <heloceinlovehotmail.fr>
2024-03-19 08:31:37 +0100

    css page recherche

--- src/Controller/SeemoreController.php
+++ src/Controller/SeemoreController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\Persistence\ManagerRegistry; // Importez ManagerRegistry
+use Doctrine\Persistence\ManagerRegistry; 
 
 class SeemoreController extends AbstractController
 {
