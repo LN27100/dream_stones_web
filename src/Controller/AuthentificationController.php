1ea9238f828082f40de2054977b8e1943e0128a6
LN27100 <heloceinlovehotmail.fr>
2024-03-19 16:50:41 +0100

    ajout reCaptcha connexion

--- src/Controller/AuthentificationController.php
+++ src/Controller/AuthentificationController.php
@@ -11,6 +11,9 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+
 
 class AuthentificationController extends AbstractController
 {
@@ -25,14 +28,48 @@ class AuthentificationController extends AbstractController
     /**
      * @Route("/login", name="app_login")
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
     {
+        // Si l'utilisateur est déjà authentifié, redirige vers la page de profil
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_profil');
+        }
+
         // Récupére les erreurs d'authentification
         $error = $authenticationUtils->getLastAuthenticationError();
         // Récupére le dernier nom d'utilisateur saisi
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        // Vérifier le reCaptcha seulement si le formulaire est soumis
+        if ($request->isMethod('POST')) {
+            // Récupérer la réponse du reCaptcha
+            $recaptchaResponse = $request->request->get('g-recaptcha-response');
+
+            // Vérifier la réponse
+            $client = HttpClient::create();
+            $response = $client->request('POST', 'https://www.google.com/recaptcha/api/siteverify', [
+                'body' => [
+                    'secret' => '6Lc3HZ4pAAAAAC_ny_ou2thqFQ8SF192EuIf13w3',
+                    'response' => $recaptchaResponse,
+                ],
+            ]);
+
+            $responseData = $response->toArray();
+
+            if (!$responseData['success']) {
+                // Le reCaptcha n'est pas validé
+                return $this->render('security/login.html.twig', [
+                    'last_username' => $lastUsername,
+                    'error' => 'reCaptcha obligatoire', // Message d'erreur
+                ]);
+            }
+        }
+
+        // Affiche la page de connexion avec les erreurs éventuelles
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
     }
 
     /**
